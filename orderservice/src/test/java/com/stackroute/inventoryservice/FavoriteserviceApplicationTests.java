ee3919d36a99ff5803f7a5a8f898f56bfc6e0fd6
Anas Deis <anas.deis@cgi.com>
2021-08-18 10:10:33 -0400

    Modified pom files

--- orderservice/src/test/java/com/stackroute/inventoryservice/FavoriteserviceApplicationTests.java
+++ orderservice/src/test/java/com/stackroute/inventoryservice/FavoriteserviceApplicationTests.java
@@ -1,13 +1,12 @@
-package com.tackroute.favoriteservice;
+package com.stackroute.inventoryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FavoriteserviceApplicationTests {
+class OrderserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
