b32f6d1f51ac16cedbf524e4e4643d3e41d73a6b
Badreddine Loulidi <badreddine.loulidi@cgi.com>
2021-08-19 10:17:18 -0400

    Added sign up form with field validation

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './signup/signup.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    SignupComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ webApp/src/app/signup/signup.component.ts
@@ -0,0 +1,42 @@
+import { CustomValidators } from '../Helpers/custom-validators';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+
+export class SignupComponent implements OnInit {
+
+  emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+  form;
+  
+  constructor(private fb: FormBuilder) {
+  }
+
+  //TODO: Fix the Validation in the UI......
+  ngOnInit() {
+    this.form = this.fb.group({
+      fullName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
+      password: [null, Validators.compose([
+        Validators.required,
+        CustomValidators.patternValidator(/\d/, {hasNumber: true}),
+        CustomValidators.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+        CustomValidators.patternValidator(/[a-z]/, { hasSmallCase: true }),
+        Validators.minLength(6)])
+      ],
+      confirmPassword: [null, Validators.compose([Validators.required])]
+    },
+    {
+      validator: CustomValidators.passwordMatchValidator
+    })
+  }
+
+  submit() {
+      console.log("Submitted")
+    }
+
+}
