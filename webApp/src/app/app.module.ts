7973b8011129f9935c97973326d26623aeaed385
soukaina <soukaina.moulaytaj@cgi.com>
2021-08-19 13:11:30 -0400

    login frontend

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ webApp/src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form;
+  
+  // message to be display if user logged in or not
+  message = '';
+
+  isLogged: boolean;
+
+  constructor(private fb : FormBuilder) {
+   
+   }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      email : ['', Validators.email],
+      password : ['', Validators.required]
+    })
+    this.isLogged = false;
+  }
+
+  onSubmit() {
+    if(this.form.value.email === '' || this.form.value.password === '') {
+      this.message = 'Email and Password should not be empty!!! Please verify details';
+    }
+   else if(this.form.invalid){
+     this.message = "Email is not valid !"
+   }
+    else {
+      const email: string = this.form.value.email;
+      console.log(email);
+      const password : string = this.form.value.password;
+      console.log(password);
+
+      this.message = "Login succesful"
+      this.isLogged = true;
+    }
+  }
+  clearForm() {
+    this.form.reset();
+  }
+
+}
