4bedaa96f075b5f12567b2da5bbf0c4623de850f
Badreddine Loulidi <badreddine.loulidi@cgi.com>
2021-08-19 11:15:23 -0400

    Added unit tests for the sign up component

--- webApp/src/app/signup/signup.component.spec.ts
+++ webApp/src/app/signup/signup.component.spec.ts
@@ -1,8 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
@@ -11,6 +9,7 @@ describe('SignupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule],
       declarations: [ SignupComponent ]
     })
     .compileComponents();
@@ -24,5 +23,65 @@ describe('SignupComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('onSubmit() should exists', () => {
+    expect(component.submit).toBeTruthy();
+  });
+
+  it('ngOnInit() should exists', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+
+  it('onSubmit() should verify form is valid or not ', () => {
+    component.form.fullName = 'Badreddine';
+    component.submit();
+    expect(component.message).toEqual('Fields should not be empty!!! Please verify details');
+  });
+
+  it('testing full name field validity', () => {
+    const fullName = component.form.controls.fullName;
+    fullName.setValue('testName');
+    expect(fullName.valid).toBeTruthy();
+  });
+
+  it('testing email field validity', () => {
+    const email = component.form.controls.email;
+    email.setValue('testEmail@gmail.com');
+    expect(email.valid).toBeTruthy();
+  });
+
+  it('testing email field not valid', () => {
+    const email = component.form.controls.email;
+    email.setValue('testEmailgmail.com');
+    expect(email.valid).toBeFalsy();
+  });
+
+  it('testing password field not valid', () => {
+    const password = component.form.controls.password;
+    password.setValue('password');
+    expect(password.valid).toBeFalsy();
+  });
+
+  it('testing password field validity', () => {
+    const password = component.form.controls.password;
+    password.setValue('Password1');
+    expect(password.valid).toBeTruthy();
+  });
+
+  it('testing confirm password field validity', () => {
+    const password = component.form.controls.password;
+    password.setValue('Password1');
+    const confirmPassword = component.form.controls.confirmPassword;
+    confirmPassword.setValue('Password1');
+    expect(confirmPassword.valid).toBeTruthy();
+  });
+
+  it('testing confirm password field not valid', () => {
+    const password = component.form.controls.password;
+    password.setValue('Password1');
+    const confirmPassword = component.form.controls.confirmPassword;
+    confirmPassword.setValue('Password123');
+    expect(confirmPassword.valid).toBeFalsy();
   });
 });
