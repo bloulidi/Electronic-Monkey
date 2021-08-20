e4fd03f3298d65918e3bf5cd80f807da1e1e914a
Anas Deis <anas.deis@cgi.com>
2021-08-20 04:11:33 -0400

    Modified service

--- userservice/src/main/java/com/stackroute/userservice/service/UserService.java
+++ userservice/src/main/java/com/stackroute/userservice/service/UserService.java
@@ -6,16 +6,20 @@ import com.stackroute.userservice.model.User;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Validated
 public interface UserService {
     User saveUser(@Valid User user) throws UserAlreadyExistsException;
-    User getUserById(int id) throws UserNotFoundException;
-    User getUserByEmail(String email) throws UserNotFoundException;
-    User deleteUser(int id) throws UserNotFoundException;
-    User updateUser(User user) throws UserNotFoundException;
+    User getUserById(@NotNull(message = "ID cannot be null") @Min(1) int id) throws UserNotFoundException;
+    User getUserByEmail(@Email(message = "Please provide a valid email address") String email) throws UserNotFoundException;
+    User deleteUser(@NotNull(message = "ID cannot be null") @Min(1) int id) throws UserNotFoundException;
+    User updateUser(@Valid User user) throws UserNotFoundException;
     List<User> getAllUsers();
-    List<User> getUsersByName(String name);
+    List<User> getUsersByName(@NotBlank(message = "Name cannot be empty") String name);
     List<User> getUsersByAdmin(boolean admin);
 }
