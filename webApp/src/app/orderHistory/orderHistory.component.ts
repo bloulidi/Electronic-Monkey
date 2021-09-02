582442ecba0896c229293b84c4cd6663aa1fb8c5
Badreddine Loulidi <badreddine.loulidi@cgi.com>
2021-09-01 22:39:22 -0400

    Connected with backend

--- webApp/src/app/orderHistory/orderHistory.component.ts
+++ webApp/src/app/orderHistory/orderHistory.component.ts
@@ -1,5 +1,6 @@
 import { Product } from './../models/Product';
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from './../services/order.service';
 
 @Component({
   selector: 'app-orderHistory',
@@ -7,27 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderHistory.component.css']
 })
 export class OrderHistoryComponent implements OnInit {
-  
-  orderProducts1 = [
-    { image: "https://www.notebookcheck.net/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1__01.JPG", name: "MacBook 2021", price: 1100},
-    { image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-white-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226036668", name: "Iphone X", price: 700},
-    { image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MD810?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1532627474032", name: "Apple 5W USB Power Adapter", price: 25},
-  ]
+  orders: any
 
-  orderProduct2 = [
-    { image: "https://www.notebookcheck.net/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1__01.JPG", name: "MacBook 2021", price: 1100},
-    { image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-white-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226036668", name: "Iphone X", price: 700},
-    { image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MD810?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1532627474032", name: "Apple 5W USB Power Adapter", price: 25},
-  ]
-  
-  order = [
-    { orderDate: "12/06/21", total: 3000, orderNumber: "H1H1H2H2", products: this.orderProducts1},
-    { orderDate: "12/08/21", total: 3000, orderNumber: "H1H1H2H2", products: this.orderProduct2}
-  ]
-
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+    this.orderService.getAllOrders().subscribe({
+      next: data => {
+        this.orders = data;
+        console.log(data);
+      },
+      error: error => {
+        if (error.status == '409') {
+          console.error("Order already exists!", error);
+        } else {
+          console.error("Failed to checkout!", error);
+        }
+      }
+    });
   }
 
 }
